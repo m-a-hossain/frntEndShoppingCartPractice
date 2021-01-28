8cd100d13fb7d46af189182a9f5f39c86f0ab27b
m-a-hossain <adam.eve007@gmail.com>
2021-01-28 22:04:29 +0600

    fixed error

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
+const remove= document.getElementById('remove')
+remove.addEventListener('click',function(){
+  document.getElementById('phon-row').remove()
+
+})
+  
+
+
+
 //functional way
+function handleProductChange(product,isIncrease){
+  const phoneInput=document.getElementById(product + '-value')
+  const phoneCount = parseInt(phoneInput.value)
+ 
+  if(isIncrease == true){
+    phoneNewCount = phoneCount+ 1
+  }
+  if(isIncrease == false && phoneCount > 0){
+    phoneNewCount = phoneCount -1
+  }
+  phoneInput.value =phoneNewCount
+  //console.log(phoneNewCount)
+  //const phonTotal= phoneNewCount * 1219
+  let phonTotal =0
+  if(product== 'phon'){
+     phonTotal= phoneNewCount * 1219
+  }
+  if(product == 'case'){
+    phonTotal= phoneNewCount * 59
+  }
+  const newPhoneCount= document.getElementById(product+'-price').innerText = phonTotal
 
+  calculateTotal()
+}
 
+function calculateTotal(){
+  const phonVal= document.getElementById('phon-value')
+  const phoneCountValue= parseInt(phonVal.value)
+
+  const caseVal= document.getElementById('case-value')
+  const caseCountValue=parseInt(caseVal.value)
+
+  const toTal = phoneCountValue * 1219 + caseCountValue *59
+  document.getElementById('subTotal').innerText = toTal
+
+  const tax=Math.round(toTal * 0.1)
+
+  document.getElementById('tax').innerText = tax
+  const grandTotal= toTal + tax
+
+ document.getElementById('total').innerText =grandTotal
+}
 /* document.getElementById('case-increase').addEventListener('click',function(){
   handleProductChange(true);// a onClick event
 })
@@ -8,11 +57,27 @@
 document.getElementById('case-decrease').addEventListener('click', function(){
   handleProductChange(false); //a onClick event
 }) */
+/* 
+function handlePhoneChange(isIncrease){
+        const phoneInput=document.getElementById('phon-value')
+        const phoneCount = parseInt(phoneInput.value)
+       
+        if(isIncrease == true){
+          phoneNewCount = phoneCount+ 1
+        }
+        if(isIncrease == false && phoneCount > 0){
+          phoneNewCount = phoneCount -1
+        }
+        phoneInput.value =phoneNewCount
+        //console.log(phoneNewCount)
+        const phonTotal= phoneNewCount * 1219
+        const newPhoneCount= document.getElementById('phon-price').innerText = phonTotal
+}
 
 function handleProductChange(isIncrease){
         const caseInput=document.getElementById('case-value')
         const caseCount = parseInt(caseInput.value)
-        /*  const caseNewCount= caseCount+ 1 */
+          //const caseNewCount= caseCount+ 1 
         if(isIncrease == true){
           caseNewCount = caseCount+ 1
         }
@@ -20,11 +85,12 @@ function handleProductChange(isIncrease){
           caseNewCount = caseCount -1
         }
         caseInput.value =caseNewCount
-          //console.log(caseNewCount)
+         
         const caseTotal= caseNewCount * 59
         const newCaseCount= document.getElementById('case_price').innerText =caseTotal
     
 }
+ */
 
 // row code 
 // document.getElementById('case-increase').addEventListener('click',function(){
